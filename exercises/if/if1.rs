3975340191f5d247899291162ce999c611abe335
Aditya Natraj <aditya.natraj@nytimes.com>
2020-08-29 20:24:22 -0400

    up to macros1

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -8,6 +8,10 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // - another function call
     // - additional variables
     // Execute `rustlings hint if1` for hints
+    match a > b {
+        true => a,
+        false => b
+    }
 }
 
 // Don't mind this for now :)
